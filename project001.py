8fa2c0f60a430913060614fd1115dca80cb55354
Vânia Margarete <95543634+vania-margarete@users.noreply.github.com>
2023-11-17 19:43:52 -0300

    Add files via upload

---
+++ project001.py
@@ -0,0 +1,22 @@
+class Heroi:
+    def init (self, nome, idade, tipo):
+        self.nome = nome
+        self.idade = idade
+        self.tipo = tipo
+
+    def atacar(self):
+        if self.tipo == "mago":
+            ataque = "usou magia"
+        elif self.tipo == "guerreiro":
+            ataque = "usou espada"
+        elif self.tipo == "monge":
+            ataque = "usou artes marciais"
+        elif self.tipo == "ninja":
+            ataque = "usou shuriken"
+        else:
+            ataque = "usou um ataque indefinido"
+
+        mensagem = "O {self.tipo} {self.nome} atacou usando {ataque}"
+        print(mensagem)
+
+
